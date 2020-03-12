1ebf4dcd4b4285b8d7743f25a04d1494e52d2d53
Marco Ris <ris@contria.com>
2020-03-12 11:28:01 +0100

    fix: Renaming assets

--- src/php/lib/enqueue-assets.php
+++ src/php/lib/enqueue-assets.php
@@ -1,8 +1,8 @@
 <?php 
 
 function gulpwordpress_assets() {
-    wp_enqueue_style( 'gulpwordpress-stylesheet', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.0', 'all' );
-    wp_enqueue_script( 'gulpwordpress-scripts', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_style( 'gulpwordpress-stylesheet', get_template_directory_uri() . '/assets/css/style.min.css', array(), '1.0.0', 'all' );
+    wp_enqueue_script( 'gulpwordpress-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array('jquery'), '1.0.0', true);
 }
 
 add_action('wp_enqueue_scripts','gulpwordpress_assets');
