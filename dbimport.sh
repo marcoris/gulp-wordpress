dff557d40bed7204eb2152b35ea408ab5ff07ddd
Marco Ris <ris@contria.com>
2021-04-15 14:08:41 +0200

    feat: Add docker command to import db dump

--- dbimport.sh
+++ dbimport.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 find ./sql/ -iname "*.sql.gz" -exec mv {} ./wwwroot/db.sql.gz \;
-vagrant ssh -c "zcat /var/www/html/db.sql.gz | mysql -u wordpress -psecret wordpress"
+# vagrant ssh -c "zcat /var/www/html/db.sql.gz | mysql -u wordpress -psecret wordpress"
+docker exec -i gulp-wordpress_db_1 mysql zcat /var/www/html/db.sql.gz | mysql -uwordpress -pwordpress wordpress
 rm ./wwwroot/db.sql.gz
