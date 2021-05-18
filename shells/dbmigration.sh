5d8bee5d69f3b884252f701ab8f1692bb9fcc3cf
Marco Ris <ris@contria.com>
2021-05-18 20:04:30 +0200

    fix: Cleanup code and add prod condition

--- shells/dbmigration.sh
+++ shells/dbmigration.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 # Get ssh username
-PRODUCTION_SSH_USER=$(grep PRODUCTION_SSH_USER .env | xargs)
-IFS='=' read -ra PRODUCTION_SSH_USER <<< "$PRODUCTION_SSH_USER"
-PRODUCTION_SSH_USER=${PRODUCTION_SSH_USER[1]}
+SSH_USER=$(grep SSH_USER .env | xargs)
+IFS='=' read -ra SSH_USER <<< "$SSH_USER"
+SSH_USER=${SSH_USER[1]}
 
 # Get ssh host
-PRODUCTION_SSH_HOST=$(grep PRODUCTION_SSH_HOST .env | xargs)
-IFS='=' read -ra PRODUCTION_SSH_HOST <<< "$PRODUCTION_SSH_HOST"
-PRODUCTION_SSH_HOST=${PRODUCTION_SSH_HOST[1]}
+SSH_HOST=$(grep SSH_HOST .env | xargs)
+IFS='=' read -ra SSH_HOST <<< "$SSH_HOST"
+SSH_HOST=${SSH_HOST[1]}
 
 # Get ssh key
-SSH_KEY=$PRODUCTION_SSH_USER
+SSH_KEY=$SSH_USER
 IFS='.' read -ra SSH_KEY <<< "$SSH_KEY"
 SSH_KEY=${SSH_KEY[0]}
 
-DATE=`date "+%Y-%m-%d-%H-%M-%s"`
-ssh -i ~/.ssh/$SSH_KEY $PRODUCTION_SSH_USER@$PRODUCTION_SSH_HOST "cd ~/public_html; rm -r ./backup/*; wp db export ./backup/live_$DATE.sql --add-drop-table"
+if [[ "$1" == "prod" ]]
+then
+	# Set public directory
+	PUBLICPATH="public_html"
+else
+	# Set public directory
+	PUBLICPATH="public_html/stage"
+fi
+
+ssh -i ~/.ssh/$SSH_KEY $SSH_USER@$SSH_HOST "cd ~/$PUBLICPATH && rm -r sql; mkdir sql; wp db export sql/$1.sql --add-drop-table"
