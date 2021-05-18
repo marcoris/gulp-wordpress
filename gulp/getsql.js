9eb64d55024fd6f7fccdb5cdc24b5bc693e4619c
Marco Ris <ris@contria.com>
2021-05-18 20:10:59 +0200

    feat: Add getSql module to get the sql file

---
+++ gulp/getsql.js
@@ -0,0 +1,40 @@
+import yargs from 'yargs';
+require('dotenv').config();
+
+var EasyFtp = require('easy-ftp');
+var ftp = new EasyFtp();
+var options, remoteString;
+
+// Check if the argument is production or staging
+if (typeof yargs.argv.prod !== 'undefined') {
+    options = {
+        host: process.env.PRODUCTION_FTP_HOST,
+        username: process.env.PRODUCTION_FTP_USER,
+        password: process.env.PRODUCTION_FTP_PASS
+    };
+    remoteString = process.env.PRODUCTION_URL;
+} else if (typeof yargs.argv.stage !== 'undefined') {
+    options = {
+        host: process.env.STAGING_FTP_HOST,
+        username: process.env.STAGING_FTP_USER,
+        password: process.env.STAGING_FTP_PASS
+    };
+    remoteString = process.env.STAGING_URL;
+} else {
+    console.log('Wrong parameter! Use --prod for production or --stage for staging');
+}
+
+// Get the sql file from production or staging
+const getSql = (done) => {
+    ftp.connect(options);
+    ftp.download('/sql', './sql', function(err) {
+        if (err) {
+            console.log(err);
+        }
+        ftp.close();
+    });
+
+    done();
+};
+
+module.exports = getSql;
