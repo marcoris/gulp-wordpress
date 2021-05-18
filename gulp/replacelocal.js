198ea9e35ca3e1a90ba827feda7218abd02b787b
Marco Ris <ris@contria.com>
2021-05-18 21:07:02 +0200

    feat: Add replace remote to local

---
+++ gulp/replacelocal.js
@@ -0,0 +1,28 @@
+import {src, dest} from 'gulp';
+import replace from 'gulp-replace';
+import rename from 'gulp-rename';
+import yargs from 'yargs';
+require('dotenv').config();
+
+var url, sqlName;
+
+// Check if the argument is production or staging
+if (typeof yargs.argv.prod !== 'undefined') {
+    url = process.env.PRODUCTION_URL;
+    sqlName = 'prod';
+} else if (typeof yargs.argv.stage !== 'undefined') {
+    url = process.env.STAGING_URL;
+    sqlName = 'stage';
+} else {
+    console.log('Wrong parameter! Use --prod for production or --stage for staging');
+}
+
+// Replace production or staging url to local url
+const replaceLocal = () => {
+    return src(`./sql/${sqlName}.sql`)
+        .pipe(replace(`${url}`, `${process.env.LOCAL_URL}`))
+        .pipe(rename('local.sql'))
+        .pipe(dest('./sql'));
+};
+
+module.exports = replaceLocal;
