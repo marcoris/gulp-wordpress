56c9c486566a78894eda19b908321ed7cfb63dbd
Marco Ris <ris@contria.com>
2021-05-19 21:57:47 +0200

    fix: getSql task

--- gulp/getsql.js
+++ gulp/getsql.js
@@ -1,9 +1,11 @@
 import yargs from 'yargs';
-require('dotenv').config();
 
 var EasyFtp = require('easy-ftp');
 var ftp = new EasyFtp();
-var options;
+
+require('dotenv').config();
+
+var options = {};
 
 // Check if the argument is production or staging
 if (typeof yargs.argv.prod !== 'undefined') {
@@ -18,19 +20,21 @@ if (typeof yargs.argv.prod !== 'undefined') {
         username: process.env.STAGING_FTP_USER,
         password: process.env.STAGING_FTP_PASS
     };
-} else {
-    console.log('Wrong parameter! Use --prod for production or --stage for staging');
 }
 
 // Get the sql file from production or staging
 const getSql = (done) => {
-    ftp.connect(options);
-    ftp.download('/sql', './sql', function(err) {
-        if (err) {
-            console.log(err);
-        }
-        ftp.close();
-    });
+    if (Object.keys(options).length > 0) {
+        ftp.connect(options);
+        ftp.download('/sql', './sql', function(err) {
+            if (err) {
+                console.log(err);
+            }
+            ftp.close();
+        });
+    } else {
+        console.log('Wrong parameter! Use --prod for production or --stage for staging');
+    }
 
     done();
 };
