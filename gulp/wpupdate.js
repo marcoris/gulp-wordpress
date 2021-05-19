fe4bd373d576e09a1ec5031c7b072f32776bff8d
Marco Ris <ris@contria.com>
2021-05-19 23:05:32 +0200

    fix: Adding dotenv

--- gulp/wpupdate.js
+++ gulp/wpupdate.js
@@ -1,5 +1,7 @@
 import run from 'gulp-run';
 
+require('dotenv').config();
+
 // Checks WP version
 const WPUpdate = () => {
     return run(`sh ./shells/wp-version-check.sh ${process.env.WP_VERSION} wwwroot/wp-includes/version.php ${process.env.WP_LOCALE} ${process.env.DOCKER_NAME}`).exec();
