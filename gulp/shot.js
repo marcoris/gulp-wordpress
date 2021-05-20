7ef3888a5017bfe4013d338e3a39d814a1010b5e
Marco Ris <ris@contria.com>
2021-05-20 18:15:20 +0200

    fix: Updated screenshot size

--- gulp/shot.js
+++ gulp/shot.js
@@ -1,8 +1,19 @@
 var webshot = require('webshot');
 require('dotenv').config();
 
+const options = {
+    screenSize: {
+        width: 1200,
+        height: 900
+    },
+    shotSize: {
+        width: 1200,
+        height: 900
+    }
+};
+
 const shot = (done) => {
-    webshot(`${process.env.LOCAL_URL}`, `./wwwroot/wp-content/themes/${process.env.DOCKER_NAME}/screenshot.png`, function(err) {
+    webshot(`${process.env.LOCAL_URL}`, `./wwwroot/wp-content/themes/${process.env.DOCKER_NAME}/screenshot.png`, options, function(err) {
         if (err) {
             console.log(err);
         }
