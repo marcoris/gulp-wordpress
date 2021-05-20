12d2bc2362823c1a4080c5bc112e03731f7c2f0f
Marco Ris <ris@contria.com>
2021-05-20 18:16:18 +0200

    fix: Rename repository

--- shells/dbimport.sh
+++ shells/dbimport.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat ./sql/local.sql | docker exec -i gulp-wordpress_db_1 mysql -uwordpress -pwordpress wordpress
+cat ./sql/local.sql | docker exec -i gulpwordpress_db_1 mysql -uwordpress -pwordpress wordpress
 rm -f ./sql/local.sql
 rm -f ./sql/prod.sql
 rm -f ./sql/stage.sql
