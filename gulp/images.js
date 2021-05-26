d9043d48458aefa01abc95a27d19747a397ff89b
Marco Ris <ris@contria.com>
2021-05-26 14:53:09 +0200

    chore: Improve images task

--- gulp/images.js
+++ gulp/images.js
@@ -3,14 +3,31 @@ import gulpif from 'gulp-if';
 import imagemin from 'gulp-imagemin';
 import pkg from '../package.json';
 import yargs from 'yargs';
+import del from 'del';
 
-const PRODUCTION = yargs.argv.prod;
+const DEPLOYING = yargs.argv.prod | yargs.argv.stage;
 
 // Images
 const images = () => {
-    return src('src/images/**/*.{jpg,jpeg,png,svg,gif}')
-        .pipe(gulpif(PRODUCTION, imagemin()))
-        .pipe(dest(`wwwroot/wp-content/themes/${pkg.name}/assets/images`));
+    // Cleanup directory
+    del(`./wwwroot/wp-content/themes/${pkg.name}/assets/images/**/*`);
+
+    // Minimize the images
+    return src('./src/images/**/*.{jpg,jpeg,png,svg,gif}')
+        .pipe(gulpif(DEPLOYING, imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 65, progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ], {
+            verbose: true
+        })))
+        .pipe(dest(`./wwwroot/wp-content/themes/${pkg.name}/assets/images`));
 };
 
 module.exports = images;
